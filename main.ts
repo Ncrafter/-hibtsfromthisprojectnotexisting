3ab903287265c5ffbfa401d02d763d309d513e0b
Ncrafter <119175392+Ncrafter@users.noreply.github.com>
2023-12-15 14:08:16 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
     ZA = 1
+    basic.pause(1000)
+    basic.clearScreen()
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    ZA = 1
+    ZA = 0
     Z += 1
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     radio.sendNumber(Z)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    ZA = 1
+    ZA = 0
     Z += -1
 })
 let Z = 0
