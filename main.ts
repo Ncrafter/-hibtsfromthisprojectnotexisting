b60ec89f4b4155690bcf94eceaba17506f223f99
Ncrafter <119175392+Ncrafter@users.noreply.github.com>
2023-12-15 14:01:51 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,25 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+    ZA = 1
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    ZA = 1
+    Z += 1
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    radio.sendNumber(Z)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    ZA = 1
+    Z += -1
+})
+let Z = 0
+let ZA = 0
+ZA = 0
+radio.setGroup(255)
+Z = 0
+basic.forever(function () {
+    if (ZA == 0) {
+        basic.showNumber(Z, 0)
+    }
+})
